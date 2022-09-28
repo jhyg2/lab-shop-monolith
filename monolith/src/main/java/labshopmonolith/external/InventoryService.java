999f1b39d9c5879cb7db3fa651b69631c73e41a8
jhyg2 <110159420+jhyg2@users.noreply.github.com>
2022-09-28 11:42:03 +0900

    commit

---
+++ monolith/src/main/java/labshopmonolith/external/InventoryService.java
@@ -0,0 +1,23 @@
+package labshopmonolith.external;
+
+import java.util.Date;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "inventory", url = "${api.url.inventory}")
+public interface InventoryService {
+    @RequestMapping(method = RequestMethod.GET, path = "/inventories/{id}")
+    public Inventory getInventory(@PathVariable("id") Long id);
+
+    @RequestMapping(
+        method = RequestMethod.PUT,
+        path = "/inventories/{id}/decreaseinventory"
+    )
+    public void decreaseInventory(
+        @PathVariable("id") Long id,
+        @RequestBody DecreaseInventoryCommand decreaseInventoryCommand
+    );
+}
